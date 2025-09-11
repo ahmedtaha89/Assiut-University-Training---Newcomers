104daa96bd01e225a9c3d77dc9b26a92aaaec54c
ahmedtaha89 <ahmed_947a@fci.kfs.edu.eg>
2025-09-12 00:45:21 +0300

    Sheet01

---
+++ Sheet01/B_BasicDataTypes.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int A;
+long long B;
+char C;
+float D;
+double E;
+cin >> A >> B >> C >> D >> E ; 
+cout  << A << endl << B << endl << C << endl << D << endl << E << endl;
+    return 0;
+}
---
+++ Sheet01/C_SimpleCalculator.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+long int X,Y;
+cin >> X >>Y;
+cout << X << " + " << Y << " = " << X+Y <<endl;
+cout << X << " * " << Y << " = " << X*Y <<endl;
+cout << X << " - " << Y << " = " << X-Y ;
+    return 0;
+}
